be825d6844e0d65329c53bbf7523f9f79b8e45de
Michael Mezzina <michaelmez39@gmail.com>
2020-05-25 23:57:45 -0400

    Working on registering a new user

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
-use actix_web::{Responder, web, };
-use argon2::hash_encoded;
+use actix_web::{http::StatusCode, web, App, HttpResponse, HttpServer, Responder};
+use argon2::{Config};
+use rand::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::sync::{ Mutex};
+type Storage = Mutex<Vec<User>>;
 
-type Storage = Vec<User>;
-
+#[derive(Serialize, Deserialize, PartialEq)]
 struct User {
     name: String,
-    password: String
-}
-fn main() {
-
-    println!("Hello, world!");
+    password: String,
 }
 
-async fn authenticate(json: web::Json<User>) -> impl Responder {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let database: Storage = Mutex::new(Vec::new()); // this is for the type checker to chill out
+        App::new()
+            .data(database)
+            .route("/register", web::post().to(register))
+            .route("/login", web::get().to(authenticate))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+async fn authenticate(_json: web::Json<User>) -> impl Responder {
     "You tried to authenticate something.".to_string()
+}
+
+pub fn hash(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config)
+        .unwrap()
+}
+
+async fn register(json: web::Json<User>, database: web::Data<Storage>) -> impl Responder {
+    let mut database = database.lock().unwrap();
+    if !database.contains(&json.0) {
+        database.push(User {
+            name: json.0.name,
+            password: hash(json.0.password.as_bytes()),
+        });
+        HttpResponse::new(StatusCode::CREATED)
+    } else {
+        HttpResponse::new(StatusCode::BAD_REQUEST)
+    }
 }
