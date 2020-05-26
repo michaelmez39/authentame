812ba2b7f0d98f5fa4622827cc658a8989806e95
Michael Mezzina <michaelmez39@gmail.com>
2020-05-26 10:00:15 -0400

    Fixed register method

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,47 @@ use argon2::{Config};
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::sync::{ Mutex};
-type Storage = Mutex<Vec<User>>;
 
-#[derive(Serialize, Deserialize, PartialEq)]
+struct Storage {
+    data: Mutex<Vec<User>>
+}
+#[derive(Serialize, Deserialize)]
 struct User {
     name: String,
     password: String,
 }
 
+impl PartialEq<User> for User {
+    fn eq(&self, other: &User) -> bool {
+        self.name == other.name
+    }
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let database: Storage = Mutex::new(Vec::new()); // this is for the type checker to chill out
+    let database: web::Data<Storage> = web::Data::new(Storage { data: Mutex::new(Vec::new()) }); // this is for the type checker to chill out
+    HttpServer::new(move || {
         App::new()
-            .data(database)
+            .app_data(database.clone())
             .route("/register", web::post().to(register))
             .route("/login", web::get().to(authenticate))
+            .route("/list", web::get().to(list))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-async fn authenticate(_json: web::Json<User>) -> impl Responder {
+async fn list(database: web::Data<Storage>) -> impl Responder {
+    let database = database.data.lock().unwrap();
+    database.iter().fold(String::new(), |acc, user| acc + &format!("User: {} and Password:{}\n", user.name, user.password))
+}
+
+async fn authenticate() -> impl Responder {
     "You tried to authenticate something.".to_string()
 }
 
-pub fn hash(password: &[u8]) -> String {
+async fn hash(password: &[u8]) -> String {
     let salt = rand::thread_rng().gen::<[u8; 32]>();
     let config = Config::default();
     argon2::hash_encoded(password, &salt, &config)
@@ -37,14 +51,15 @@ pub fn hash(password: &[u8]) -> String {
 }
 
 async fn register(json: web::Json<User>, database: web::Data<Storage>) -> impl Responder {
-    let mut database = database.lock().unwrap();
+    let mut database = database.data.lock().unwrap();
     if !database.contains(&json.0) {
-        database.push(User {
+        let new_user = User {
             name: json.0.name,
-            password: hash(json.0.password.as_bytes()),
-        });
+            password: hash(json.0.password.as_bytes()).await,
+        };
+        database.push(new_user);
         HttpResponse::new(StatusCode::CREATED)
     } else {
-        HttpResponse::new(StatusCode::BAD_REQUEST)
+        HttpResponse::new(StatusCode::CONFLICT)
     }
 }
